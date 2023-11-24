e1490d4747356e180a937a862db6bba028ce588d
gjlander <garrett.jlander@gmail.com>
2023-11-24 14:41:35 +0100

    converted a recipe api into json files in a format we can use, and tested migrating some. Can migrate the entire api to make our recipe blog feels full-it has 300 or so meals

--- schemas/blogPageSchema.js
+++ schemas/blogPageSchema.js
@@ -1,9 +1,24 @@
 import { Schema } from "mongoose";
 // import cardSchema from "./cardSchema.js";
+import ingListSchema from "./ingListSchema.js";
 
 const blogPageSchema = new Schema({
     //card no longer used
     // card: cardSchema,
+    title: { type: String, maxLength: 100 },
+    category: String,
+    region: String,
+    ingList: [ingListSchema],
+    steps: [String],
+    text: {
+        type: String,
+        maxLength: 255,
+        default: "Simply delicious",
+    },
+    button: {
+        type: String,
+        default: "To Recipe",
+    },
     imgUrl: {
         type: String,
         maxLength: 510,
@@ -12,11 +27,6 @@ const blogPageSchema = new Schema({
             "must be a valid URL",
         ],
     },
-    title: { type: String, maxLength: 100 },
-    text: { type: String, maxLength: 255 },
-    button: String,
-    ingList: [String],
-    steps: [String],
 });
 
 export default blogPageSchema;
