864b26c34556cf25acfad35f304ab68f74817f5e
gjlander <garrett.jlander@gmail.com>
2023-11-25 14:01:23 +0100

    added new endpoint for getting all recipe pages to sync with algolia

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -7,6 +7,7 @@ import {
     oneBlog,
     editBlog,
     findBlogsFromUser,
+    getBlogPages,
     addBlogPage,
     deleteBlogPage,
     addHero,
@@ -25,10 +26,14 @@ blogRouter.route("/").get(allBlogs).post(createBlog);
 blogRouter.route("/:id").get(oneBlog).put(ClerkExpressRequireAuth(), editBlog);
 //updated to get based on clerkId
 blogRouter.route("/user/:id").get(findBlogsFromUser);
-blogRouter.route("/:blogId/:pageId").get(singlePage);
+blogRouter.route("/singlepage/:blogId/:pageId").get(singlePage);
 
 //didn't end up needing them, but hey, I learned something
-blogRouter.route("/blogPages/:id").post(addBlogPage).delete(deleteBlogPage);
+blogRouter
+    .route("/blogPages/:id")
+    .get(getBlogPages)
+    .post(addBlogPage)
+    .delete(deleteBlogPage);
 blogRouter.route("/hero/:id").post(addHero).delete(deleteHero);
 
 blogRouter.route("/migrate/:id").post(migrateMeals);
