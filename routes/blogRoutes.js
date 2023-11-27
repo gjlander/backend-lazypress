1d75ae7a0aa16f2d180fd93d9a8b69168bd51f84
priyankapasrija <104025698+priyankapasrija@users.noreply.github.com>
2023-11-27 19:58:54 +0100

    git pulled

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,13 +2,13 @@ import express from "express";
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
 import {
-    allBlogs,
-    createBlog,
-    oneBlog,
-    editBlog,
-    findBlogsFromUser,
-    getClerkAuth,
-    clerkPostTest,
+  allBlogs,
+  createBlog,
+  oneBlog,
+  editBlog,
+  findBlogsFromUser,
+  getClerkAuth,
+  clerkPostTest,
 } from "../controllers/blogControllers.js";
 
 const blogRouter = express.Router();
@@ -20,8 +20,8 @@ blogRouter.route("/:id").get(oneBlog).put(ClerkExpressRequireAuth(), editBlog);
 //updated to get based on clerkId
 blogRouter.route("/user/:id").get(findBlogsFromUser);
 blogRouter
-    .route("/protected/endpoint")
-    .get(ClerkExpressRequireAuth(), getClerkAuth)
-    .post(ClerkExpressRequireAuth(), clerkPostTest);
+  .route("/protected/endpoint")
+  .get(ClerkExpressRequireAuth(), getClerkAuth)
+  .post(ClerkExpressRequireAuth(), clerkPostTest);
 
 export default blogRouter;
